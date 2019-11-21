56104ba02ad50d6d18b1acea744d4c1f98cde1cd
handong <455808519@qq.com>
2019-11-21 10:23:19 +0800

    代码规范

--- cmd_toutiao.py
+++ cmd_toutiao.py
@@ -7,16 +7,18 @@ import queue
 from objects import News
 
 # 数据源url
-start_url = 'https://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1&max_behot_time={}&max_behot_time_tmp={}&tadrequire=true&as={}&cp={}'
-url = 'https://www.toutiao.com'
+base_data_url = 'https://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1&max_behot_time={}&max_behot_time_tmp={}&tadrequire=true&as={}&cp={}'
+base_source_url = 'https://www.toutiao.com'
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
 }
 cookies = {'tt_webid': '6649949084894053895'}  # cookies可从浏览器中查找，为了避免被头条禁止爬虫
+max_behot_time = 0  # 默认从0开始，接口返回中获取下一个值
 
 # 已爬取的新闻队列
 news_queue = queue.Queue(100);
+
 
 def get_as_cp():
     """ 获取as和cp参数，参考今日头条中的加密js文件：home_4abea46.js"""
@@ -41,22 +43,24 @@ def get_as_cp():
     return {'as': 'A1' + s + e[-3:], 'cp': e[0:3] + r + 'E1'}
 
 
-def get_data(url, headers, cookies):
+def get_data(data_url: str):
     """获取网页数据"""
     try:
-        r = requests.get(url, headers=headers, cookies=cookies)
+        r = requests.get(data_url, headers=headers, cookies=cookies)
         return json.loads(r.text)
     except:
-        raise "get data from toutiao failed"
+        print("get data from toutiao failed")
+        raise
 
 
-def main(max_behot_time):
+def crawl():
+    global max_behot_time
     # 获取as和cp参数值
     ascp = get_as_cp()
     # 格式化url
-    data_url = start_url.format(max_behot_time, max_behot_time, ascp['as'], ascp['cp'])
+    data_url = base_data_url.format(max_behot_time, max_behot_time, ascp['as'], ascp['cp'])
     # 获取数据
-    http_result = get_data(data_url, headers, cookies)
+    http_result = get_data(data_url)
     data = http_result['data']
     for data_index in range(len(data)):
         item = data[data_index]
@@ -64,7 +68,7 @@ def main(max_behot_time):
         source = item['source']
         source_url = item['source_url']
         if 'https' not in source_url:
-            source_url = url + source_url
+            source_url = base_source_url + source_url
         news = News(title, source, source_url)
         news_queue.put(news)
     # 获取下一个链接的max_behot_time参数的值
@@ -72,18 +76,9 @@ def main(max_behot_time):
 
 
 if __name__ == '__main__':
-    max_behot_time = '0'  # 初始为0，从初始数据中得到下一链接的此参数值
     print("欢迎来到今日头条命令行版，按page_down开始摸鱼")
     while True:
-        try:
-            if keyboard.is_pressed('esc'):
-                print("好的伙计，我们努力工作")
-                exit()
-            elif keyboard.is_pressed('page_down'):
-                if(news_queue.empty()):
-                    main(max_behot_time)
-                news_queue.get().print();
-            else:
-                pass
-        except:
-            break
+        keyboard.wait("page down")
+        if news_queue.empty():
+            crawl()
+        news_queue.get(True, 1).print()
