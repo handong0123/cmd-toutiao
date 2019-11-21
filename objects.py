344802d9e89252e27b274d78396e24daaadf153c
handong <455808519@qq.com>
2019-11-21 10:23:30 +0800

    修改print

--- objects.py
+++ objects.py
@@ -8,10 +8,10 @@ class News:
     source_url = ""
     source = ""
 
-    def __init__(self, title, author,url):
+    def __init__(self, title, author, url):
         self.title = title
         self.source = author
         self.source_url = url
 
     def print(self):
-        print("title:" + self.title + " source" + self.source + " source_url:" + self.source_url)
+        print("{}\n{}\t{}".format(self.title, self.source, self.source_url))
